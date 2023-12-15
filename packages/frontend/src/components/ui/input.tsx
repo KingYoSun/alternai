9533262f838e43e4f197ecd3fe2b4821f7a102cb
KingYoSun <nocking257838@gmail.com>
2023-12-15 21:42:48 +0900

    fix:input padding

--- packages/frontend/src/components/ui/input.tsx
+++ packages/frontend/src/components/ui/input.tsx
@@ -25,7 +25,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={inputType}
           className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background pl-3 pr-9 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            `flex h-10 w-full rounded-md border border-input bg-background pl-3 ${
+              type === "password" ? "pr-9" : "pr-3"
+            } py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
             className,
           )}
           ref={ref}
